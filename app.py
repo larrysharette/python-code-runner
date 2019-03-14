5db3d455ec09f70850656442b98b40414f117042
Larry Sharette <larrysharette@a-celectric.com>
2019-03-13 21:17:48 -0700

    added cors

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from subprocess import Popen, PIPE
 import sys
 import os
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/', methods=['POST'])
 def index():
